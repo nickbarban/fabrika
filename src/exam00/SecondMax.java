0cefd2b79f03298c51ad8afcf8fee0cb76599a6c
Nick Barban <nn.barban@gmail.com>
2015-07-13 22:41:09 +0300

    changed algorithm

--- src/exam00/SecondMax.java
+++ src/exam00/SecondMax.java
@@ -1,51 +1,55 @@
 package exam00;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
-
-public class SecondMax {
+public final class SecondMax {
 	
 	public static int findSecondMaxInt(int[] arr) {
-		boolean allElemetstAreTheSame = false;
+		int allElemetstAreTheSame = 1;
+		int firstMax = arr[0];
 		int secondMax = 0;
 		
 		if (arr.length > 1){
-			for (int i = 0; i < arr.length; i++) {
-				for (int j = i+1; j < arr.length; j++) {
-					if (arr[j] > arr[i]) {
-						int tmp = arr[i];
-						arr[i] = arr[j];
-						arr[j] = tmp;
-						allElemetstAreTheSame = false;
-					} else if (arr[j] == arr[i]){
-						allElemetstAreTheSame = true;
+			secondMax = arr[1];			
+			for (int i = 1; i < arr.length; i++) {				
+				if (firstMax == arr[i]) {
+					allElemetstAreTheSame *= 1;
+				} else if (firstMax < arr[i]) {
+					allElemetstAreTheSame *= 0;
+					secondMax = firstMax;
+					firstMax = arr[i];
+					//System.out.println("swapped first and arr[" + i + "]");
+				} else {
+					if (secondMax < arr[i]) {
+						secondMax = arr[i];
+						//System.out.println("swapped second and arr[" + i + "]");
 					}
-				} // for j
+					allElemetstAreTheSame *= 0;
+				}
+				//System.out.println(firstMax + "||" + secondMax + "||" + allElemetstAreTheSame);
+				
 			} // for i
 			
-			if (allElemetstAreTheSame) {
+			if (allElemetstAreTheSame == 1) {
 				throw new IllegalArgumentException("all elements of array are the same");
 			}
 			
-			for (int k = 0; k < arr.length - 1; k++) {
-				if (arr[k] != arr[k + 1]) {
-					secondMax = arr[k + 1];
-					break;
-				}
-			} // for k
+			
+			
 			
 		} else {			
 			if (arr.length == 0) {
 				throw new IllegalArgumentException("array has no element");	
 			} else {
 				throw new IllegalArgumentException("array has only one element");
-			}
+		}
+		
 		}
 		
 		return secondMax;		
 	} // findSecondMaxInt
+
+	/*public static void main(String[] args) {
+		System.out.println(findSecondMaxInt(new int[]{8,8,15,15,9,8,10,1,50,65}));
+	}*/
 	
 	
 	
